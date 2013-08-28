684cc69b3a4752f5e5e7407a38b98419f83c6b8e
joelongstreet <joelongstreet@gmail.com>
2013-08-27 19:25:59 -0500

    dropping in chaplin boilerplate... as well as the handlebars library

---
+++ assets/js/application.js
@@ -0,0 +1,9 @@
+define(['chaplin'], function(Chaplin) {
+  'use strict';
+
+  var Application = Chaplin.Application.extend({
+    title: 'Gutenberg'
+  });
+
+  return Application;
+});
---
+++ assets/js/views/base/collection-view.js
@@ -0,0 +1,12 @@
+define([
+  'handlebars',
+  'chaplin/base/view',
+], function(Chaplin, View){
+  'use strict';
+
+  var CollectionView = Chaplin.CollectionView.extend({
+    getTemplateFunction : View.prototype.getTemplateFunction
+  });
+
+  return CollectionView;
+});
