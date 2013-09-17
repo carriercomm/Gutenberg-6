74fa96524a15c33041dc699be8777b5518288c73
joelongstreet <joelongstreet@gmail.com>
2013-09-17 10:29:06 -0500

    socket powered models

--- assets/js/models/base/model.js
+++ assets/js/models/base/model.js
@@ -3,7 +3,18 @@ define([
 ], function(Chaplin) {
   'use strict';
 
-  var Model = Chaplin.Model.extend({});
+  var Model = Chaplin.Model.extend({
+    listen : function(){
+      var model = this;
+      socket.request(this.url, this.params, function(results){
+        model.set(results);
+      });
+
+      socket.on('message', function(message, other){
+        model.set(message.data);
+      });
+    }
+  });
 
   return Model;
 });
