8482a2bfe75aead700ba106fe273bdb5e15b2276
joelongstreet <joelongstreet@gmail.com>
2013-09-19 08:19:28 -0500

    fix newsletter timestamp

--- assets/js/controllers/publication-controller.js
+++ assets/js/controllers/publication-controller.js
@@ -92,8 +92,7 @@ define([
 
     createNewsletter : function(publication){
       var date          = new Date();
-      var time          = date.getTime();
-      var defaultTitle  = publication.get('title') + ' - ' + months[date.getMonth()] + ' ' + days[date.getDay()] + ' ' + date.getFullYear() + ' ' + time;
+      var defaultTitle  = publication.get('title') + ' - ' + months[date.getMonth()] + ' ' + days[date.getDay()] + ' ' + date.getFullYear();
       var newsletter    = new Newsletter();
       newsletter.url    = '/newsletter/create';
 
