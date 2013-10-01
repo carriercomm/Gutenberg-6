06c0d62ac66d420a6a18f44a26a0f535027f02da
joelongstreet <joelongstreet@gmail.com>
2013-10-01 15:06:34 -0500

    now rendering pre views in the context of a smart (yet css naked) iframe

--- assets/js/controllers/base/controller.js
+++ assets/js/controllers/base/controller.js
@@ -7,7 +7,10 @@ define([
   var Controller = Chaplin.Controller.extend({
     beforeAction : function(){
       Chaplin.Controller.prototype.beforeAction.apply(this, arguments);
-      this.compose('site', SiteView)
+
+      // Always compose the site view unless looking at a newlsetter preview
+      if(arguments[1].action == 'preview' && arguments[1].controller == 'newsletter'){}
+      else {this.compose('site', SiteView)};
     }
   });
 
--- assets/js/views/stories-pre-view.js
+++ assets/js/views/stories-pre-view.js
@@ -6,16 +6,13 @@ define([
 ], function(Chaplin, ejs, View, StoryView){
   'use strict';
 
-  var view = View.extend({
-    itemView      : StoryView,
-    template      : '<div id="stories-wrapper"></div>',
-    listSelector  : '#stories-wrapper'
-  });
+  var view = View.extend({});
 
 
   view.prototype.initialize = function(){
     Chaplin.View.prototype.initialize.apply(this, arguments);
     this.subscribeEvent('channels_registered', this.registerTemplates);
+    this.listenTo(this.model, 'change', this.render);
     this.listenTo(this.collection, 'change', this.render);
   };
 
@@ -23,28 +20,30 @@ define([
   view.prototype.registerTemplates = function(channels){
     var index             = this.options.params.templateIndex;
     this.options.template = ejs.compile(channels[index].templates.preview);
+    this.registered       = true;
     this.render();
   };
 
 
   view.prototype.render = function(){
-    Chaplin.View.prototype.render.apply(this, arguments);
-    
-    // The regular toJSON method cannot be used here since i'm using
-    // it to blacklist attributes for socket saves. 
-    var stories = [];
-    for(var i=0; i<this.collection.models.length; i++){
-      var story  = _.clone(this.collection.models[i]);
-      var images = story.get('images');
 
-      story.attributes.images = images.length ? images.toJSON() : []
-      stories.push(story.attributes);
+    if(this.registered){
+      // The regular toJSON method cannot be used here since i'm using
+      // it to blacklist attributes for socket saves. 
+      var stories = [];
+      for(var i=0; i<this.collection.models.length; i++){
+        var story  = _.clone(this.collection.models[i]);
+        var images = story.get('images');
+
+        //story.attributes.images = images.length ? images.toJSON() : []
+        stories.push(story.attributes);
+      }
+
+      var passedOptions = { stories : stories };
+      var html          = this.options.template(passedOptions);
+
+      $(this.el).html(html);
     }
-
-    var passedOptions = { stories : stories };
-    var html          = this.options.template(passedOptions);
-
-    $(this.el).html(html);
   };
 
   return view;
