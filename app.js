6da65344648ae0ffddf44a04a2026282b957ea6a
joelongstreet <joelongstreet@gmail.com>
2013-10-04 08:34:02 -0500

    add node performance metrics

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
+if(process.env.NODE_TIME_KEY){
+  require('nodetime').profile({
+    accountKey  : process.env.NODE_TIME_KEY,
+    appName     : 'Gutenberg'
+  });
+}
+
 // Start sails and pass it command line arguments
 require('sails').lift(require('optimist').argv);
