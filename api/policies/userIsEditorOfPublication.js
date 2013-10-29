7c0c31dabe9457a8a7ca04d3b5c5deb0a6923ec6
joelongstreet <joelongstreet@gmail.com>
2013-10-29 14:41:55 -0500

    slight syntactic bug fix

--- api/policies/userIsEditorOfPublication.js
+++ api/policies/userIsEditorOfPublication.js
@@ -2,16 +2,17 @@ module.exports = function (req, res, next){
 
   var username = req.session.username || '';
 
-  User.findOneByUsername(req.session.username).exec(function(err, userModel){
+  User.findOneByUsername(username).exec(function(err, userModel){
     if(err) console.log(err);
+    var user = userModel || {};
 
-    if(userModel.isMaster) next();
+    if(user.isMaster) next();
     else {
       // If requesting a single publication
       if(req.params.id){
         Publication.findOneById(req.params.id).exec(function(err, publicationModel){
           var owners = publicationModel.owners || [];
-          if(owners.indexOf(userModel.id) != -1){ next() }
+          if(owners.indexOf(user.id) != -1){ next() }
           else { res.send(401) }
         });
       }
