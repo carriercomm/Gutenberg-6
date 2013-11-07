564f47c2dc843fd0742532eb745055ff9bf3d258
jlongstreet <jlongstreet@6830664f-4833-0410-9d60-bbd6aaeb8672>
2013-11-07 15:14:12 +0000

    further perfecting this gruntfile
    git-svn-id: https://svn.vmldev.com/repos/vml/gutenberg.vml.com/trunk@20608 6830664f-4833-0410-9d60-bbd6aaeb8672

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,41 +1,10 @@
 module.exports = function (grunt) {
 
-  var cssFilesToInject = [
-    'linker/**/*.css'
-  ];
-
-  var jsFilesToInject = [
-    'linker/js/vendor/require.js'
-  ];
-
-  var templateFilesToInject = [
-    'linker/**/*.hbs'
-  ];
-
-
-
-  // Modify css file injection paths to use 
-  cssFilesToInject = cssFilesToInject.map(function (path) {
-    return '.tmp/public/' + path;
-  });
-
-  // Modify js file injection paths to use 
-  jsFilesToInject = jsFilesToInject.map(function (path) {
-    return '.tmp/public/' + path;
-  });
-
-  templateFilesToInject = templateFilesToInject.map(function (path) {
-    return 'assets/' + path;
-  });
-
-
   // Get path to core grunt dependencies from Sails
   var depsPath = grunt.option('gdsrc') || 'node_modules/sails/node_modules';
   grunt.loadTasks(depsPath + '/grunt-contrib-clean/tasks');
   grunt.loadTasks(depsPath + '/grunt-contrib-copy/tasks');
   grunt.loadTasks(depsPath + '/grunt-contrib-concat/tasks');
-  grunt.loadTasks(depsPath + '/grunt-sails-linker/tasks');
-  grunt.loadTasks(depsPath + '/grunt-contrib-jst/tasks');
   grunt.loadTasks(depsPath + '/grunt-contrib-watch/tasks');
   grunt.loadTasks(depsPath + '/grunt-contrib-uglify/tasks');
   grunt.loadTasks(depsPath + '/grunt-contrib-cssmin/tasks');
@@ -50,8 +19,9 @@ module.exports = function (grunt) {
         options : {
           baseUrl         : '.tmp/working/js',
           name            : 'start',
-          mainConfigFile  : '.tmp/working/js/require-config.js',
-          out             : '.tmp/public/js/require-optimized.js'
+          mainConfigFile  : '.tmp/working/js/config/require.js',
+          out             : '.tmp/public/js/production.js',
+          urlArgs         : ''
         }
       }
     },
@@ -87,30 +57,13 @@ module.exports = function (grunt) {
 
     clean : {
       dev   : ['.tmp/public/**', '.tmp/working/**'],
-      prod  : ['.tmp/public/**', '.tmp/working/**']
-    },
-
-    jst: {
-      dev: {
-        options: {
-          templateSettings: {
-            //interpolate: /\{\{(.+?)\}\}/g
-          }
-        },
-        files: {
-          '.tmp/public/jst.js': templateFilesToInject
-        }
-      }
+      prod  : ['.tmp/working/**']
     },
 
     concat : {
-      js : {
-        src  : jsFilesToInject,
-        dest : '.tmp/public/concat/production.js'
-      },
       css: {
-        src  : cssFilesToInject,
-        dest : '.tmp/public/concat/production.css'
+        src  : ['.tmp/working/styles/**/*'],
+        dest : '.tmp/working/concat/production.css'
       }
     },
 
@@ -123,73 +76,8 @@ module.exports = function (grunt) {
 
     cssmin : {
       dist : {
-        src  : ['.tmp/public/concat/production.css'],
-        dest : '.tmp/public/min/production.css'
-      }
-    },
-
-    'sails-linker': {
-
-      devJs : {
-        options : {
-          startTag  : '<!--SCRIPTS-->',
-          endTag    : '<!--SCRIPTS END-->',
-          fileTmpl  : '<script data-main="/linker/js/start.js" src="%s"></script>',
-          appRoot   : '.tmp/public'
-        },
-        files : {
-          'views/**/*.ejs' : jsFilesToInject
-        }
-      },
-
-      prodJs : {
-        options : {
-          startTag  : '<!--SCRIPTS-->',
-          endTag    : '<!--SCRIPTS END-->',
-          fileTmpl  : '<script data-main="/linker/js/start.js" src="%s"></script>',
-          appRoot   : '.tmp/public'
-        },
-        files : {
-          'views/**/*.ejs': ['.tmp/public/js/optimized.js']
-        }
-      },
-
-      devStyles : {
-        options : {
-          startTag  : '<!--STYLES-->',
-          endTag    : '<!--STYLES END-->',
-          fileTmpl  : '<link rel="stylesheet" href="%s">',
-          appRoot   : '.tmp/public'
-        },
-
-        files : {
-          'views/**/*.ejs' : cssFilesToInject
-        }
-      },
-
-      prodStyles: {
-        options: {
-          startTag  : '<!--STYLES-->',
-          endTag    : '<!--STYLES END-->',
-          fileTmpl  : '<link rel="stylesheet" href="%s">',
-          appRoot   : '.tmp/public'
-        },
-        files: {
-          'views/**/*.ejs' : ['.tmp/public/min/production.css']
-        }
-      },
-
-      // TODO - come back to this and implement
-      devTpl : {
-        options : {
-          startTag  : '<!--TEMPLATES-->',
-          endTag    : '<!--TEMPLATES END-->',
-          fileTmpl  : '<script type="text/javascript" src="%s"></script>',
-          appRoot   : '.tmp/public'
-        },
-        files : {
-          'views/**/*.ejs' : ['.tmp/public/jst.js']
-        }
+        src  : ['.tmp/working/concat/production.css'],
+        dest : '.tmp/public/styles/production.css'
       }
     },
 
@@ -199,38 +87,25 @@ module.exports = function (grunt) {
       },
       assets : {
         files   : ['assets/**/*'],
-        tasks   : ['compileAssets', 'linkAssets']
+        tasks   : ['compileAssets']
       }
     }
   });
-
-
-  grunt.registerTask('default', [
-    'compileAssets',
-    'linkAssets',
-    'watch'
-  ]);
+  
+  // Check static assets for changes
+  grunt.registerTask('default', ['compileAssets', 'watch']);
 
   grunt.registerTask('compileAssets', [
     'clean:dev',
-    'jst:dev',
     'copy:dev'
-  ]);
-
-  grunt.registerTask('linkAssets', [
-    'sails-linker:devJs',
-    'sails-linker:devStyles',
-    'sails-linker:devTpl'
   ]);
 
   grunt.registerTask('prod', [
     'clean:dev',
-    'jst:dev',
     'copy:prod',
-    'requirejs',
+    'concat:css',
     'cssmin',
-    'sails-linker:prodJs',
-    'sails-linker:prodStyles',
-    'sails-linker:devTpl'
+    'requirejs',
+    'clean:prod'
   ]);
 };
