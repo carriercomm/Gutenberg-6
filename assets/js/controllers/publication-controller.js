a8c351d016af57c5bec7bc16fd258f388d7ce8d5
jlongstreet <jlongstreet@6830664f-4833-0410-9d60-bbd6aaeb8672>
2013-11-07 14:41:24 +0000

    adding socket bindings to all collections
    git-svn-id: https://svn.vmldev.com/repos/vml/gutenberg.vml.com/trunk@20404 6830664f-4833-0410-9d60-bbd6aaeb8672

--- assets/js/controllers/publication-controller.js
+++ assets/js/controllers/publication-controller.js
@@ -1,20 +1,26 @@
 define([
   'controllers/base/controller',
   'models/base/collection',
+  'models/base/model',
   'views/publications-view'
-], function(Controller, Collection, CollectionView){
+], function(Controller, Collection, Model, CollectionView){
   'use strict';
 
   var Publications = Controller.extend({
     showAll : function(params){
-      this.collection     = new Collection()
+      this.collection     = new Collection();
       this.collection.url = '/publication';
-      this.collection.fetch();
+      this.collection.listen();
 
       this.view       = new CollectionView({
         collection  : this.collection,
         region      : 'main'
       });
+    },
+
+    showOne : function(params){
+      this.model      = new Model();
+      this.model.url  = '/publication';
     }
   });
 
--- assets/js/models/base/collection.js
+++ assets/js/models/base/collection.js
@@ -5,7 +5,23 @@ define([
   'use strict';
 
   var Collection = Chaplin.Collection.extend({
-    model: Model
+    model   : Model,
+    listen  : function(){
+      var collection = this;
+
+      socket.request(this.url, {}, function(results){
+        collection.add(results);
+      });
+
+      socket.on('message', function(message){
+        if(message.verb == 'create') collection.add(message.data)
+        else if(message.verb == 'destroy') collection.remove(message.id)
+        else if(message.verb == 'update'){
+          var model = collection.get(message.id)
+          model.set(message.data);
+        }
+      });
+    }
   });
 
   return Collection;
