28844c9b39e1ec647ac5d787270688345c328f62
jlongstreet <jlongstreet@6830664f-4833-0410-9d60-bbd6aaeb8672>
2013-11-07 14:41:51 +0000

    enabling push state
    git-svn-id: https://svn.vmldev.com/repos/vml/gutenberg.vml.com/trunk@20406 6830664f-4833-0410-9d60-bbd6aaeb8672

--- assets/js/initialize.js
+++ assets/js/initialize.js
@@ -1,34 +1,34 @@
 require.config({
-    baseUrl     : './js/',
-    paths       : {
-        'jquery'      : '../vendor/jquery',
-        'underscore'  : '../vendor/underscore',
-        'handlebars'  : '../vendor/handlebars',
-        'backbone'    : '../vendor/backbone',
-        'chaplin'     : '../vendor/chaplin',
-        'text'        : '../vendor/requirejs-text',
-        'sailsio'     : '../vendor/sails.io',
-        'socketio'    : '../vendor/socket.io'
+  baseUrl     : '/js/',
+  paths       : {
+    'jquery'      : '/vendor/jquery',
+    'underscore'  : '/vendor/underscore',
+    'handlebars'  : '/vendor/handlebars',
+    'backbone'    : '/vendor/backbone',
+    'chaplin'     : '/vendor/chaplin',
+    'text'        : '/vendor/requirejs-text',
+    'sailsio'     : '/vendor/sails.io',
+    'socketio'    : '/vendor/socket.io'
+  },
+  shim        : {
+    sailsio       : {
+        exports       : 'sailsio'
     },
-    shim        : {
-        sailsio     : {
-            exports     : 'sailsio'
-        },
-        socketio    : {
-            exports     : 'io'
-        },
-        underscore  : {
-            exports     : '_'
-        },
-        backbone    : {
-            deps        : ['underscore', 'jquery'],
-            exports     : 'Backbone'
-        },
-        handlebars  : {
-            exports     : 'Handlebars'
-        }
+    socketio      : {
+        exports       : 'io'
     },
-    urlArgs     : 'bust=' +  (new Date()).getTime()
+    underscore    : {
+        exports       : '_'
+    },
+    backbone      : {
+        deps          : ['underscore', 'jquery'],
+        exports       : 'Backbone'
+    },
+    handlebars    : {
+        exports       : 'Handlebars'
+    }
+  },
+  urlArgs         : 'bust=' +  (new Date()).getTime()
 });
 
 require(['application', 'routes', 'socketio'], function(Application, routes, io){
@@ -37,7 +37,7 @@ require(['application', 'routes', 'socketio'], function(Application, routes, io)
   require(['sailsio'], function(sailsio){
     window.socket = io.connect()
     socket.on('connect', function(){
-        new Application({ routes : routes, controllerSuffix : '-controller' });
+        new Application({ routes : routes, controllerSuffix : '-controller', root : '/ui' });
     });
   });
 });
