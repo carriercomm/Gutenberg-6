64f6e20b4b27420a853038c840197506e986b164
jlongstreet <jlongstreet@6830664f-4833-0410-9d60-bbd6aaeb8672>
2013-11-07 14:44:51 +0000

    bringing modal functionality into the image view and out of a controller
    git-svn-id: https://svn.vmldev.com/repos/vml/gutenberg.vml.com/trunk@20426 6830664f-4833-0410-9d60-bbd6aaeb8672

--- assets/js/views/image-view.js
+++ assets/js/views/image-view.js
@@ -1,15 +1,19 @@
 define([
   'chaplin',
+  'bootstrap',
+  'models/base/model',
   'views/base/view',
+  'views/imageCrop-view',
   'text!templates/image.hbs'
-], function(Chaplin, View, template){
+], function(Chaplin, bootstrap, Model, View, CropView, template){
   'use strict';
 
   var view = View.extend({
     template      : template,
     className     : 'image-container',
     events        : {
-      'click .delete' : 'deleteImage'
+      'click .delete' : 'deleteImage',
+      'click .edit'   : 'editImage'
     }
   });
 
@@ -21,6 +25,44 @@ define([
     var earl = '/image/destroy/' + id;
 
     $.ajax({ url : '/image/destroy/' + id });
+  };
+
+
+  view.prototype.editImage = function(e) {
+    Chaplin.View.prototype.render.apply(this, arguments);
+    e.preventDefault();
+
+    var model = new Model();
+    model.url = '/image/' + $(e.target).data('id');
+
+    // Placeholder until I add crop settings to the Newsletter model
+    var croppableItems = [
+      { domId : 'postImage', title : 'Post Image', width : '300', height : '150', cropOptions : { aspectRatio : '2' }},
+      { domId : 'emailImage', title : 'Email Image', width : '200', height : '200', cropOptions : { aspectRatio : '1' }},
+      { domId : 'freeImage', title : 'Free Image' },
+      { domId : 'vmlCom', title : 'VML.com Image', width : '400', height : '400', cropOptions : { aspectRatio : 1 }}
+    ]
+    model.set('croppableItems', croppableItems);
+    // End Placeholder
+
+    model.fetch({
+      success : function(){
+        var cropView = new CropView({
+          autoRender  : true,
+          model       : model,
+          region      : 'main',
+          className   : 'modal',
+          attributes  : {
+            'id'      : 'image-cropper'
+          }
+        });
+
+        $('#image-cropper').modal();
+        $('#image-cropper').on('hidden.bs.modal', function(){
+          $('#image-cropper').remove();
+        });
+      }
+    });
   };
 
   return view;
