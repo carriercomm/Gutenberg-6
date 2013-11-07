c83852184c7a9cef888b68513905fc1ef5e790f7
jlongstreet <jlongstreet@6830664f-4833-0410-9d60-bbd6aaeb8672>
2013-11-07 14:42:50 +0000

    fix typo, adding some comments so i remember to do something later
    git-svn-id: https://svn.vmldev.com/repos/vml/gutenberg.vml.com/trunk@20413 6830664f-4833-0410-9d60-bbd6aaeb8672

--- assets/js/models/base/collection.js
+++ assets/js/models/base/collection.js
@@ -6,7 +6,7 @@ define([
 
   var Collection = Chaplin.Collection.extend({
     model   : Model,
-    parms   : {},
+    params  : {},
     listen  : function(){
       var collection = this;
 
@@ -15,13 +15,22 @@ define([
       });
 
       // I think this handler might have to be way more sophisticated
+      // TODO:
+        // Drop socket when collection is dropped, i thought chaplin said it would unbind
+          // everything if the registration was done correctly
+        // Do param matching either here, or on the server. I'm thinking it would be easier
+          // to do it on the client, but better to do it on the server
       socket.on('message', function(message){
         if(message.verb == 'create') collection.add(message.data)
         else if(message.verb == 'destroy') collection.remove(message.id)
         else if(message.verb == 'update'){
           var model = collection.get(message.id)
-          model.set(message.data);
+          if(model){
+            model.set(message.data);
+          }
         }
+
+        console.log(message);
       });
     }
   });
