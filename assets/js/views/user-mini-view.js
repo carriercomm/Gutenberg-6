1dbed5bae9d2e9a68068701a66beb10217c891d9
jlongstreet <jlongstreet@6830664f-4833-0410-9d60-bbd6aaeb8672>
2013-11-07 15:15:50 +0000

    adding make and revoke access buttons inf publication single view to manage users
    git-svn-id: https://svn.vmldev.com/repos/vml/gutenberg.vml.com/trunk@20618 6830664f-4833-0410-9d60-bbd6aaeb8672

--- assets/js/views/user-mini-view.js
+++ assets/js/views/user-mini-view.js
@@ -10,24 +10,61 @@ define([
     className     : 'list-group-item',
     template      : template,
     events        : {
-      'click .deauthorize' : 'deauthorize'
+      'click .revoke-editor'  : 'revokeEditor',
+      'click .revoke-owner'   : 'revokeOwner',
+      'click .make-editor'    : 'makeEditor',
+      'click .make-owner'     : 'makeOwner'
     }
   });
 
 
   view.prototype.render = function(){
+    // Show auth or deauth button?
+    var publication = this.options.publication;
+    var owners      = publication.get('owners');
+    var editors     = publication.get('editors');
+    var myId        = this.model.get('id');
+
+    if(owners.indexOf(myId) != -1) this.model.set('isOwner', true);
+    if(editors.indexOf(myId) != -1) this.model.set('isEditor', true);
+
     Chaplin.View.prototype.render.apply(this, arguments);
+
+    // Set the URL to the User
     var earl = '/ui/user/' + this.model.get('id');
     $(this.el).attr('href', earl);
   };
 
 
-  view.prototype.deauthorize = function(e){
+  view.prototype.makeEditor = function(e){
+    e.preventDefault();
+    e.stopPropagation();
+
+    console.log('auth');
+  };
+
+
+  view.prototype.revokeEditor = function(e){
     e.preventDefault();
     e.stopPropagation();
 
     var confirm = window.confirm('Are you sure you want to deauth this user');
-    //if(confirm) this.model.destroy();
+  };
+
+
+  view.prototype.makeOwner = function(e){
+    e.preventDefault();
+    e.stopPropagation();
+
+    console.log('make');
+  };
+
+
+  view.prototype.revokeOwner = function(e){
+    e.preventDefault();
+    e.stopPropagation();
+
+    console.log('revoke');
   };
 
   return view;
--- assets/js/views/users-view.js
+++ assets/js/views/users-view.js
@@ -12,5 +12,20 @@ define([
     listSelector  : '.list-group'
   });
 
+
+  view.prototype.initialize = function(){
+    Chaplin.CollectionView.prototype.initialize.apply(this, arguments);
+    this.options.publication.listenTo(this.options.publication, 'change', this.doSumthin)
+  };
+
+
+  view.prototype.initItemView = function(model){
+    return new this.itemView({
+      model           : model,
+      collectionView  : this,
+      publication     : this.options.publication
+    });
+  };
+
   return view;
 });
