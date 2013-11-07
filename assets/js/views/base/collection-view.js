347c14ccb374ac0395bbdf63fc9aa7201ab1d606
jlongstreet <jlongstreet@6830664f-4833-0410-9d60-bbd6aaeb8672>
2013-11-07 14:40:42 +0000

    dropping in chaplin boilerplate... as well as the handlebars library
    git-svn-id: https://svn.vmldev.com/repos/vml/gutenberg.vml.com/trunk@20400 6830664f-4833-0410-9d60-bbd6aaeb8672

--- assets/js/views/base/collection-view.js
+++ assets/js/views/base/collection-view.js
@@ -7,4 +7,9 @@ define([
   var CollectionView = Chaplin.CollectionView.extend({
     getTemplateFunction : View.prototype.getTemplateFunction
   });
+<<<<<<< HEAD
+=======
+
+  return CollectionView;
+>>>>>>> dropping in chaplin boilerplate... as well as the handlebars library
 });
