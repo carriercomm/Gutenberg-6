9a7c706f81b7c81c3f19c15e2234aeb59bd36a87
jlongstreet <jlongstreet@6830664f-4833-0410-9d60-bbd6aaeb8672>
2013-11-07 14:50:57 +0000

    hack to make socket bound collections still work when not even unbound.... soooo many events happening
    git-svn-id: https://svn.vmldev.com/repos/vml/gutenberg.vml.com/trunk@20465 6830664f-4833-0410-9d60-bbd6aaeb8672

--- assets/js/models/base/collection.js
+++ assets/js/models/base/collection.js
@@ -15,24 +15,31 @@ define([
         if(next) next(results);
       });
 
+      // TODO: this needs to be unbound when the collection is disposed of
       socket.on('message', function(message){
-        if(message.verb == 'create') {
-          // Loop over the message data and decide id the params match
-          var truths  = 0;
-          var keys    = Object.keys(collection.params);
 
-          for(var i = 0; i<keys.length; i++){
-            if(message.data[keys[i]] == collection.params[keys[i]]) truths++
+        // This needs to be here because the collection is not unbound
+        if(collection.length){
+
+          if(message.verb == 'create') {
+            // Loop over the message data and decide id the params match
+            var truths  = 0;
+            var keys    = Object.keys(collection.params);
+
+            for(var i = 0; i<keys.length; i++){
+              if(message.data[keys[i]] == collection.params[keys[i]]) truths++
+            }
+            if(truths >= keys.length) collection.add(message.data);
+
+          } else if(message.verb == 'destroy') {
+            collection.remove(message.id)
+          } else if(message.verb == 'update'){
+            // TODO: this could potentially blow up if a collection containing
+            // the same model id of a different model type is used
+            var model = collection.get(message.id)
+            if(model) model.set(message.data);
           }
-          if(truths >= keys.length) collection.add(message.data);
 
-        } else if(message.verb == 'destroy') {
-          collection.remove(message.id)
-        } else if(message.verb == 'update'){
-          // TODO: this could potentially blow up if a collection containing
-          // the same model id of a different model type is used
-          var model = collection.get(message.id)
-          if(model) model.set(message.data);
         }
       });
     }
