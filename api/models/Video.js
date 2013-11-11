7c3d1bc2605742b0e5096d47a5e9f2dd804734dd
joelongstreet <joelongstreet@gmail.com>
2013-11-11 13:02:52 -0600

    go fetch video thumbnails beforeCreation of the model

--- api/models/Video.js
+++ api/models/Video.js
@@ -6,10 +6,16 @@
  * @docs		:: http://sailsjs.org/#!documentation/models
  */
 
+var request = require('request');
+
 module.exports = {
 
   attributes: {
     url : {
+      type      : 'STRING',
+      defaultsTo: ''
+    },
+    image_url : {
       type      : 'STRING',
       defaultsTo: ''
     },
@@ -27,7 +33,34 @@ module.exports = {
   beforeCreate : function(props, next){
     Video.find({ story_id : props.story_id}).exec(function(err, collection){
       props.sort_index = collection.length;
-      next();
+
+      if(props.url){
+        // If url is passed in, go get the video image
+        var requestEarl = '';
+
+        // YouTube
+        if(props.url.indexOf('youtube.com') != -1 ){
+          var splits = props.url.split('?v=');
+          props.image_url = 'http://img.youtube.com/vi/' + splits[1] + '/1.jpg';
+          next();
+        }
+
+        // Vimeo
+        if(props.url.indexOf('vimeo.com') != -1 ){
+          var splits = props.url.split('/')
+          var vidId  = splits[splits.length - 1];
+          var requestEarl = 'http://vimeo.com/api/v2/video/' + vidId + '.json';
+
+          request(requestEarl, function(err, res, body){
+            if(err) props.image_url = '/images/vimeo.jpg'
+            else props.image_url = JSON.parse(body)[0].thumbnail_medium
+            next();
+          });
+        }
+      } else {
+        props.image_url = '/images/video.jpg';
+        next();
+      }
     });
   }
 
